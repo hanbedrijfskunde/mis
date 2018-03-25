2ab734c74a290c580c7e06853df177a634046868
Witek ten Hove <wfth69@gmail.com>
2018-03-25 18:08:16 +0200

    Update charts.js

--- workshops/stats/lectures/normdist/js/charts.js
+++ workshops/stats/lectures/normdist/js/charts.js
@@ -1,41 +1,122 @@
 class histchart {
-  constructor (id = 'chart1', title = 'Histogram', data = [1, 5, 6]) {
+  constructor(id = 'chart1', title = 'Histogram', data = [1, 5, 6]) {
     this.id = id;
     this.title = title;
     this.data = data;
   }
-    chart() {
-      return Highcharts.chart(this.id, {
+  chart() {
+    return Highcharts.chart(this.id, {
+      title: {
+        text: 'Normale verdeling'
+      },
+      xAxis: [{
         title: {
-          text: 'Normale verdeling'
+          text: this.title
         },
+        min: -15,
+        max: 15,
+        alignTicks: false,
+        opposite: false
+      }],
+
+      yAxis: [{
+        title: {
+          text: 'Histogram'
+        },
+        opposite: false
+      }],
+      series: [{
+        name: 'Histogram',
+        type: 'histogram',
+        data: this.data,
+        id: 's1',
+        xAxis: 0,
+        yAxis: 0,
+        baseSeries: 's1',
+        zIndex: -1,
+        color: 'OrangeRed'
+      }]
+    })
+  }
+};
+
+class bellchart {
+  constructor(id = 'chart1', title = 'Gauss Curve', data = [1, 5, 6]) {
+    this.id = id;
+    this.title = title;
+    this.data = data;
+  }
+  chart() {
+    var pointsInInterval = 5;
+      return Highcharts.chart(this.id, {
+
+        title: {
+          text: null
+        },
+
+        legend: {
+          enabled: false
+        },
+
         xAxis: [{
+          title: {
+            text: 'Data'
+          },
+          min: -15,
+          max: 15,
+          visible: true
+        }, {
           title: {
             text: this.title
           },
           min: -15,
           max: 15,
-          alignTicks: false,
-          opposite: false
+          opposite: true,
+          visible: true
         }],
 
         yAxis: [{
           title: {
-            text: 'Histogram'
+            text: 'Data'
           },
-          opposite: false
+          visible: false
+        }, {
+          title: {
+            text: this.title
+          },
+          opposite: true,
+          visible: true
         }],
+        plotOptions: {
+        series: {
+            color: 'rgb(255,69,0,0.4)',
+            fillColor: 'rgb(255,69,0,0)'
 
-        series: [{
+        }
+    },
+    series: [{
           name: 'Histogram',
           type: 'histogram',
-          data: this.data,
-          id: 's1',
           xAxis: 0,
           yAxis: 0,
           baseSeries: 's1',
           zIndex: -1,
-          color: 'OrangeRed'
+          color: 'SkyBlue'
+        },
+        {
+          name: 'Bell curve asd',
+          type: 'bellcurve',
+          data: this.data,
+          id: 's1',
+          xAxis: 1,
+          yAxis: 1,
+          pointsInInterval: pointsInInterval,
+          intervals: 4,
+          baseSeries: 's1',
+          zIndex: -1,
+          marker: {
+            enabled: false
+          }
         }]
       })
     }
