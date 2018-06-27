6384565cb11dedb37694b6727b0d1bfdc3c77553
witusj <wfth69@gmail.com>
2018-06-27 20:04:44 +0200

    Update

--- workshops/infocap/sticker.R
+++ workshops/infocap/sticker.R
@@ -4,7 +4,7 @@ library(tidyverse)
 
 p <- ggplot(data.frame(x = c(-4, 6)), aes(x)) + 
   mapply(function(mean, sd, col) {
-    stat_function(fun = dnorm, args = list(mean = mean, sd = sd), col = col)
+    stat_function(fun = dnorm, args = list(mean = mean, sd = sd), col = col, size = 2)
   }, 
   # enter means, standard deviations and colors here
   mean = c(-1, 1, 3), 
@@ -16,5 +16,5 @@ p <- p + theme_void() + theme_transparent()
 
 sticker(p, package="InfoCap", p_color = "black", p_size=8, s_x=1, s_y=.75, s_width=1,
         s_height=.8, filename="logo.png", h_fill="#F9E79F",
-        h_color="#E74C3C", spotlight=TRUE)
+        h_color="#E74C3C", spotlight=FALSE)
 
